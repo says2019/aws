84cd1e4f4a28cf2855bac927d42487ee7fcff963
says2019 <sattisudheeshreddy@gmail.com>
2025-07-28 17:20:03 +0530

    stepfunctions

--- LambdaFunction/AwsLambda.tf
+++ LambdaFunction/AwsLambda.tf
@@ -1,4 +1,15 @@
 # Zip the Lambda function
+variable "lambda_configs" {
+  type = map(string)
+  default = {
+    notify_new_record       = "notify_new_record.py"
+    purchase_capacity_block = "purchase_capacity_block.py"
+    return_task_token       = "return_task_token.py"
+    set_request_status      = "set_request_status.py"
+    submit_approval_request = "submit_approval_request.py"
+  }
+}
+
 data "archive_file" "lambda_zips" {
   for_each    = var.lambda_configs
   type        = "zip"
