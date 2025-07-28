df49c3f5d64452327bd958304ec2343d710fb821
says2019 <sattisudheeshreddy@gmail.com>
2025-07-28 17:03:51 +0530

    stepfunctions

--- LambdaFunction/variables.tf
+++ LambdaFunction/variables.tf
@@ -1,11 +1,10 @@
 variable "lambda_configs" {
-  type = list(string)
-  default = [
-    "notify_new_record",
-    "purchase_capacity_block",
-    "return_task_token",
-    "send_approval_email",
-    "set_request_status",
-    "submit_approval_request"
-  ]
+  type = map(string)
+  default = {
+    notify_new_record       = "notify_new_record.py"
+    purchase_capacity_block = "purchase_capacity_block.py"
+    return_task_token       = "return_task_token.py"
+    set_request_status      = "set_request_status.py"
+    submit_approval_request = "submit_approval_request.py"
+  }
 }
