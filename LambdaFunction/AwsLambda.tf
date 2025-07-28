3fec2b91102c610d4250747cb99fe2d381612ee5
says2019 <sattisudheeshreddy@gmail.com>
2025-07-28 14:22:53 +0530

    stepfunctions

--- LambdaFunction/AwsLambda.tf
+++ LambdaFunction/AwsLambda.tf
@@ -8,20 +8,26 @@ data "archive_file" "init" {
 # S3 Bucket
 resource "aws_s3_bucket" "cqpocsbucket" {
   bucket = "quickcloudpocsbucket001"
-  acl    = "private"
-
+  force_destroy = true  # optional: allows deletion even if non-empty
   tags = {
     Name = "cqpocsbucket-1"
   }
 }
 
+resource "aws_s3_bucket_acl" "private_acl" {
+  bucket = aws_s3_bucket.cqpocsbucket.id
+  acl    = "private"
+}
+
 # Upload zip file to S3
-resource "aws_s3_bucket_object" "lambda_zip" {
+resource "aws_s3_object" "lambda_zip" {
   bucket = aws_s3_bucket.cqpocsbucket.id
   key    = "notify-new-record.zip"
   source = data.archive_file.init.output_path
   etag   = filemd5(data.archive_file.init.output_path)
+  content_type = "application/zip"
 }
+
 
 # IAM Role for Lambda
 resource "aws_iam_role" "lambda_role" {
@@ -38,14 +44,15 @@ resource "aws_iam_role_policy" "lambda_policy" {
 
 # Lambda Function
 resource "aws_lambda_function" "test_lambda" {
-  function_name = "notify_new_record"
-  s3_bucket     = aws_s3_bucket.cqpocsbucket.id
-  s3_key        = aws_s3_bucket_object.lambda_zip.key
-  role          = aws_iam_role.lambda_role.arn
-  handler       = "notify-new-record.handler" # update if your function name is different
-  runtime       = "python3.8"
+  function_name    = "notify_new_record"
+  s3_bucket        = aws_s3_bucket.cqpocsbucket.id
+  s3_key           = aws_s3_object.lambda_zip.id
+  role             = aws_iam_role.lambda_role.arn
+  handler          = "notify-new-record.handler"
+  runtime          = "python3.8"
   source_code_hash = data.archive_file.init.output_base64sha256
 }
+
 
 # Output
 output "pythonLambdaArn" {
