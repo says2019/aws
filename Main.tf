e124f6bd3b795cc3ea45f0b96168c3dd56fb52ea
says2019 <sattisudheeshreddy@gmail.com>
2025-08-27 14:35:34 +0530

    stepfunctions

--- Main.tf
+++ Main.tf
@@ -1,30 +1,68 @@
-  terraform{
-    # Terraform version at the time of writing this post
-    required_version = ">= 1.8.5"
-
-#     backend "s3"{
-#         bucket = "cloudquickpocsbackendtf"
-#         key = "quickcloudpocsbackend.tfstate"
-#         region = "us-east-1"
-#     }
-
+terraform {
+  backend "s3" {
+    bucket         = "my-terraform-states"
+    key            = "iam/role/terraform.tfstate"
+    region         = "ap-south-1"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
   }
 
-
-  provider "random" {}
-
-  provider "aws"{
-      region = "us-east-1"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
   }
+}
 
-  #Create Aws Python lambda function
-  module "awslambdafunction"{
-    source = "./LambdaFunction"
-  }
+provider "aws" {
+  region = "ap-south-1"
+}
+
+# IAM Role
+resource "aws_iam_role" "example_role" {
+  name = "example-iam-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action    = "sts:AssumeRole",
+        Effect    = "Allow",
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+# Attach Policy
+resource "aws_iam_role_policy_attachment" "example_attach" {
+  role       = aws_iam_role.example_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
+}
+
+output "role_name" {
+  value = aws_iam_role.example_role.name
+}
 
 
-  #Create aws stepfunction to Inovke aws Lambda function
-  module "awsstepfunction"{
-   source =  "./StepFunction"
-   pythonfunctionapparn = module.awslambdafunction.pythonLambdaArn
-  }
+
+#   provider "random" {}
+#
+#   provider "aws"{
+#       region = "us-east-1"
+#   }
+#
+#   #Create Aws Python lambda function
+#   module "awslambdafunction"{
+#     source = "./LambdaFunction"
+#   }
+#
+#
+#   #Create aws stepfunction to Inovke aws Lambda function
+#   module "awsstepfunction"{
+#    source =  "./StepFunction"
+#    pythonfunctionapparn = module.awslambdafunction.pythonLambdaArn
+#   }
