acd44077b4f487131e49535060d20e1d0f09c3f4
says2019 <sattisudheeshreddy@gmail.com>
2025-08-27 14:47:05 +0530

    stepfunctions

--- Main.tf
+++ Main.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "my-terraform-states"
     key            = "iam/role/terraform.tfstate"
-    region         = "ap-south-1"
+    region         = "us-east-1"
     dynamodb_table = "terraform-locks"
     encrypt        = true
   }
